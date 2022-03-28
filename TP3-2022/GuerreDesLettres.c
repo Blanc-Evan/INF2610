06a0fc42a69aa15fc8cd614e88b478b5e5528460
Evan BLANC <evanblanc.pro@icloud.com>
2022-03-28 17:06:11 -0400

    avancement tp3

--- TP3-2022/GuerreDesLettres.c
+++ TP3-2022/GuerreDesLettres.c
@@ -9,27 +9,41 @@
 sem_t mutex;
 sem_t product;
 char* tampon;
+unsigned nbl = 0;
 
 // fonction exécutée par les producteurs
 void* producteur( void* pid) {
    srand(time(NULL));
    char c;
+   unsigned int  nbCharEcrit = 0;
    while(1) {
       c='A' + rand()%26;
-
+      sem_wait(&product);
+      tampon[nbCharEcrit] = c;
+      nbCharEcrit++;
+      sleep(1);
+      sem_post(&product);
    }
    return NULL;             
 }
+
 // fonction exécutée par les consommateurs
 void* consommateur(void *cid) {
    while(1) {
+      sem_wait(&mutex);
+      if (nbl == 0) sem_wait(&product);
+      nbl++;
+      sem_post(&mutex);
+      sleep(1);
+      // lecture
 
-
+      sem_wait(&mutex);
+      nbl--;
+      if (nbl == 0) sem_post(&product);
+      sem_post(&mutex);
    }
   return NULL;   
-  
 }
-
 
 // fonction main
 int main(int argc, char* argv[]) {
@@ -37,8 +51,8 @@ int main(int argc, char* argv[]) {
       le nombre de producteurs, le nombre de consommateurs
       et la taille du tampon.*/
     
-   unsigned nbProducteurs = argv[1];
-   unsigned nbConsomateurs = argv[2];
+   unsigned int nbProducteurs = argv[1];
+   unsigned int nbConsomateurs = argv[2];
    tampon = malloc(argv[3]);
 
    const u_int8_t NUMBER_OF_THREADS = nbProducteurs + nbConsomateurs;
@@ -57,6 +71,5 @@ int main(int argc, char* argv[]) {
    for (int i = 0; i < NUMBER_OF_THREADS; i++) {
        pthread_join(tids[i], NULL);
    }
-
    return 0;
 }
