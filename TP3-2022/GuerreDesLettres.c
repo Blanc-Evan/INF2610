41848fe715a329e95e1146f146e059bd5e6a6604
Evan BLANC <evanblanc.pro@icloud.com>
2022-04-01 16:01:12 -0400

    grosse avancee tp3

--- TP3-2022/GuerreDesLettres.c
+++ TP3-2022/GuerreDesLettres.c
@@ -5,24 +5,44 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 sem_t mutex;
-sem_t product;
+sem_t libre;
+sem_t occupe;
+
 char* tampon;
-unsigned nbl = 0;
+unsigned int nbProducteurs=0;
+unsigned int nbConsomateurs=0;
 unsigned int  nbCharEcrit = 0;
+unsigned int  nbCharLu = 0;
+unsigned int tailleTampon;
+unsigned int ic=0, ip=0;
+bool flag_de_fin = false;
+
+void write_zero() {
+   for (int i=0; i < nbConsomateurs; i++) {
+      tampon[i] = '0';
+   }
+}
+
+void sig_handler(int signum){
+   flag_de_fin = true;
+}
 
 // fonction exécutée par les producteurs
 void* producteur( void* pid) {
-   srand(time(NULL));
    char c;
    while(1) {
-      c='A' + rand()%26;
-      sem_wait(&product);
-      tampon[nbCharEcrit] = c;
+      sem_wait(&libre);
+      sem_wait(&mutex);
+      printf("caractère écrit : %c\n", c);
+      tampon[ip] = 'A' + rand()%26;
+      ip = (ip+1) % tailleTampon;
       nbCharEcrit++;
-      sleep(1);
-      sem_post(&product);
+      sem_post(&mutex);
+      sem_post(&occupe);
+      if(flag_de_fin) break;
    }
    return NULL;             
 }
@@ -30,17 +50,15 @@ void* producteur( void* pid) {
 // fonction exécutée par les consommateurs
 void* consommateur(void *cid) {
    while(1) {
+      sem_wait(&occupe);
       sem_wait(&mutex);
-      if (nbl == 0) sem_wait(&product);
-      nbl++;
-      sem_post(&mutex);
-      sleep(1);
-      // lecture
-      printf("caractères lu : %s \n", tampon);
+      char c = tampon[ic];
+      if(c == '0') break;
+      printf("caractère lu : %c \n", c);
+      ic = (ic+1) % tailleTampon;
+      nbCharLu++;
       sem_wait(&mutex);
-      nbl--;
-      if (nbl == 0) sem_post(&product);
-      sem_post(&mutex);
+      sem_post(&libre);
    }
   return NULL;   
 }
@@ -50,26 +68,43 @@ int main(int argc, char* argv[]) {
    /* Les paramètres du programme sont, dans l’ordre :
       le nombre de producteurs, le nombre de consommateurs
       et la taille du tampon.*/
+
+   signal(SIGALRM,sig_handler);
+   srand(time(NULL));
     
-   unsigned int nbProducteurs = (unsigned int)argv[1];
-   unsigned int nbConsomateurs = (unsigned int)argv[2];
-   tampon = malloc((unsigned int)argv[3]);
+   nbProducteurs = atoi(argv[1]);
+   nbConsomateurs = atoi(argv[2]);
+   tailleTampon = atoi(argv[3]);
+   tampon = malloc(tailleTampon);
 
    const u_int8_t NUMBER_OF_THREADS = nbProducteurs + nbConsomateurs;
-   pthread_t tids[NUMBER_OF_THREADS];
+   pthread_t tids[nbProducteurs];
+   pthread_t cids[nbConsomateurs];
    sem_init(&mutex, 0, 1);
-   sem_init(&product, 0, 1);
+   sem_init(&libre, 0, 1);
+   sem_init(&occupe, 0, 1);
 
    for (int i = 0; i < nbProducteurs; i++) {
-       pthread_create(&tids[i], NULL, producteur, NULL);
+       pthread_create(&tids[i], NULL, producteur, (void*)i);
    }
 
    for (int i = 0; i < nbConsomateurs; i++) {
-       pthread_create(&tids[i], NULL, consommateur, NULL);
+       pthread_create(&cids[i], NULL, consommateur, (void*)i);
    }
 
-   for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+   alarm(1);
+
+   for (int i = 0; i < nbProducteurs; i++) {
        pthread_join(tids[i], NULL);
    }
+
+   write_zero();
+
+   for (int i = 0; i < nbConsomateurs; i++) {
+             pthread_join(cids[i], NULL);
+   }
+
+   printf("Les consomateurs ont fait en tout %d écritures et les lecteurs ont fait %d lectures \n", nbCharEcrit, nbCharLu);
+
    return 0;
 }
