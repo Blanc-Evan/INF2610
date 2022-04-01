d5897be5b59fe71f52433c9c114ea5e49f14ce37
Evan BLANC <evanblanc.pro@icloud.com>
2022-04-01 16:40:39 -0400

    reparations

--- TP3-2022/GuerreDesLettres.c
+++ TP3-2022/GuerreDesLettres.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <signal.h>
 
 sem_t mutex;
 sem_t libre;
@@ -33,32 +34,41 @@ void sig_handler(int signum){
 // fonction exécutée par les producteurs
 void* producteur( void* pid) {
    char c;
+   int nbEcritures = 0;
    while(1) {
       sem_wait(&libre);
       sem_wait(&mutex);
-      printf("caractère écrit : %c\n", c);
       tampon[ip] = 'A' + rand()%26;
       ip = (ip+1) % tailleTampon;
       nbCharEcrit++;
+      nbEcritures++;
       sem_post(&mutex);
       sem_post(&occupe);
-      if(flag_de_fin) break;
+      if(flag_de_fin) {
+         printf("Le producteur %d a écrit %d lettres dans le tampon \n", pid, nbEcritures);
+         pthread_exit(NULL);
+      }
    }
    return NULL;             
 }
 
 // fonction exécutée par les consommateurs
 void* consommateur(void *cid) {
+   int nbConsomations = 0;
    while(1) {
       sem_wait(&occupe);
       sem_wait(&mutex);
       char c = tampon[ic];
-      if(c == '0') break;
-      printf("caractère lu : %c \n", c);
+      //printf("caractère lu : %c \n", c);
       ic = (ic+1) % tailleTampon;
+      nbConsomations++;
       nbCharLu++;
       sem_wait(&mutex);
       sem_post(&libre);
+      if(c == '0') {
+         printf("Le consomateur %d a consomé %d lettres dans le tampon \n", cid, nbConsomations);
+         pthread_exit(NULL);
+      }
    }
   return NULL;   
 }
@@ -71,18 +81,18 @@ int main(int argc, char* argv[]) {
 
    signal(SIGALRM,sig_handler);
    srand(time(NULL));
-    
+   
    nbProducteurs = atoi(argv[1]);
    nbConsomateurs = atoi(argv[2]);
    tailleTampon = atoi(argv[3]);
-   tampon = malloc(tailleTampon);
+   tampon = calloc(tailleTampon, sizeof(char));
 
-   const u_int8_t NUMBER_OF_THREADS = nbProducteurs + nbConsomateurs;
    pthread_t tids[nbProducteurs];
    pthread_t cids[nbConsomateurs];
+
    sem_init(&mutex, 0, 1);
-   sem_init(&libre, 0, 1);
-   sem_init(&occupe, 0, 1);
+   sem_init(&libre, 0, tailleTampon);
+   sem_init(&occupe, 0, 0);
 
    for (int i = 0; i < nbProducteurs; i++) {
        pthread_create(&tids[i], NULL, producteur, (void*)i);
@@ -95,16 +105,16 @@ int main(int argc, char* argv[]) {
    alarm(1);
 
    for (int i = 0; i < nbProducteurs; i++) {
-       pthread_join(tids[i], NULL);
+      pthread_join(tids[i], NULL);
    }
 
    write_zero();
 
    for (int i = 0; i < nbConsomateurs; i++) {
-             pthread_join(cids[i], NULL);
+      pthread_join(cids[i], NULL);
    }
 
-   printf("Les consomateurs ont fait en tout %d écritures et les lecteurs ont fait %d lectures \n", nbCharEcrit, nbCharLu);
+   printf("Les consomateurs ont fait en tout %d productions et les consomateurs ont fait %d consomations \n", nbCharEcrit, nbCharLu);
 
    return 0;
 }
