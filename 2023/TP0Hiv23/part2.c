b1872c8d185201ad9935c81597342b1d6053794a
Evan BLANC <evanblanc.pro@icloud.com>
2023-01-26 15:29:40 -0500

    fin tp0

--- 2023/TP0Hiv23/part2.c
+++ 2023/TP0Hiv23/part2.c
@@ -10,16 +10,35 @@
 // Si besoin, ajouter ici les directives d'inclusion et les déclarations globales
 // -------------------------------------------------
 // TODO
-
+#include <string.h>
 // -------------------------------------------------
-void part21 ()
+void part21 () // On se rend bien compte que write sera executé avant car pas de '\n' qui flusherait le buffer de printf
 {
  // TODO
+    char* line1 = "77dbcb01f571f1c32e196c3a7d27f62e (printed using write) \n";
+    char* line2 = "77dbcb01f571f1c32e196c3a7d27f62e (printed using fprintf)";
+    
+    for(int i=0 ; i<strlen(line2) ; i++) {
+        printf("%c", line2[i]);
+    }
+
+    write(0, line1, strlen(line1));
 }
 
 void part22 ()
 {
  // TODO
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    char* line1 = "77dbcb01f571f1c32e196c3a7d27f62e (printed using write) \n";
+    char* line2 = "77dbcb01f571f1c32e196c3a7d27f62e (printed using fprintf)";
+    
+    for(int i=0 ; i<strlen(line2) ; i++) {
+        printf("%c", line2[i]);
+    }
+
+    write(0, line1, strlen(line1));
+
 }
 
 
